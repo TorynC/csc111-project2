e0931e8889bff2957b12422cebaee9c24238f1f6
Dan <iambighill@gmail.com>
2024-03-17 16:28:42 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+""""CSC111 Project 2: Movie Recommendation System
+
+The part where we create ask questions and run recommending system and give recommanding movies.
+Simply, get input and run the code and give the output.
+This is not the file where we putting system. This is for calling system.
+"""
