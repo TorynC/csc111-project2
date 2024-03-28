449e689105764193886a4579f15a960ff7f04666
lightningkoala <36724057+lightningkoala@users.noreply.github.com>
2024-03-28 15:41:09 -0400

    .

--- data_computations.py
+++ data_computations.py
@@ -2,7 +2,6 @@
 import csv
 
 
-
 class Movie:
     """Class that represents one movie."""
 
@@ -18,14 +17,23 @@ class Movie:
     stars: set[str]
     gross: int
 
-
     def __init__(self, title, release_year, age_rate, imdb_rating, meta_score, runtime, genre, description, director,
                  stars, gross):
+        self.title = title
+        self.release_year = release_year
+        self.age_rate = age_rate
+        self.imdb_rating = imdb_rating
+        self.meta_score = meta_score
+        self.runtime = runtime
+        self.genre = genre
+        self.description = description
+        self.director = director
+        self.stars = stars
+        self.gross = gross
 
 
 def csv_to_object(file_name: str) -> list[Movie]:
     """A function that will take in a review file and output a list of Movie objects."""
-
 
     output_lst = []
     with open(file_name, 'r') as file:
