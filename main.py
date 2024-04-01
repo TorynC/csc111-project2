09271248101f558a32f2dd48ec1c5d063aec296a
Toryn <torync03@gmail.com>
2024-04-01 13:51:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 """"CSC111 Winter 2024 Project 2: Movie Recommendation System
 
+Module Description
+==================
 This Python module is the main module where the program is run.
 
 Copyright and Usage Information
 ===============================
-This file is provided solely for the personal and private use of the CSC111 course department
-at the University of Toronto St. George campus. All forms of distribution of this code,
-whether as given or with any changes, are strictly prohibited. For more information on
-copyright for CSC111 project materials, please consult our Course Syllabus.
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
 
 This file is Copyright (c) 2024 Toryn Chua, Jacob Keluthara, Yeonjun Kim, Dan Kim.
 """
