ef5425698e07fa98e6753182de62c0de63545959
Toryn <torync03@gmail.com>
2024-04-01 14:56:21 -0400

    m

--- recommendation_system.py
+++ recommendation_system.py
@@ -17,8 +17,9 @@ please consult our Course Syllabus.
 
 This file is Copyright (c) 2024 Toryn Chua, Jacob Keluthara, Yeonjun Kim, Dan Kim.
 """
-import pygame
+from typing import Any
 import sys
+import pygame
 import pygame_gui
 from weighted_decision import recommendation_system
 
@@ -30,11 +31,11 @@ YELLOW = (222, 181, 34)
 
 SCREEN = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("IMDb Movie Recommendation System")
-titlefont = pygame.font.SysFont("arialblack", 37)
-descriptionfont = pygame.font.SysFont("arialblack", 20)
+TITLE_FONT = pygame.font.SysFont("arialblack", 37)
+DESCRIPTION_FONT = pygame.font.SysFont("arialblack", 20)
 CLOCK = pygame.time.Clock()
 MANAGER = pygame_gui.UIManager(SCREEN_SIZE)
-to_be_printed = []
+TO_BE_PRINTED = []
 TEXT_INPUT1 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((300, 140), (400, 50),
                                                   manager=MANAGER, object_id="text1"))
 TEXT_INPUT2 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((300, 300), (400, 50),
@@ -69,14 +70,14 @@ def is_integer(userinput: int | str) -> bool:
 class StateManager:
     """Class that will create the object to allow transition between different pages of the
     program"""
-    def __init__(self, currentstate):
+    def __init__(self, currentstate: Any) -> None:
         self.currentstate = currentstate
 
     def get_state(self) -> None:
         """returns the current page being displayed"""
         return self.currentstate
 
-    def set_state(self, state) -> None:
+    def set_state(self, state: str) -> None:
         """changes the current page"""
         self.currentstate = state
 
@@ -91,13 +92,13 @@ class Pages:
     display: pygame.Surface
     state_manager: StateManager
 
-    def __init__(self, display, state_manager):
+    def __init__(self, display: pygame.Surface, state_manager: StateManager) -> None:
         self.display = display
         self.state_manager = state_manager
 
     def run(self) -> None:
         """Abstract method that will run each page"""
-        raise NotImplemented
+        raise NotImplementedError
 
 
 class Results(Pages):
@@ -106,31 +107,31 @@ class Results(Pages):
     def run(self) -> None:
         """Method that will run display the results page of the GUI"""
         self.display.fill(BLACK)
-        text1surface = draw_text("Results", titlefont, YELLOW)
+        text1surface = draw_text("Results", TITLE_FONT, YELLOW)
         text1rect = text1surface.get_rect(center=(SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 10))
         self.display.blit(text1surface, text1rect)
 
-        exitmessage = draw_text("Press BACKSPACE to exit", descriptionfont, YELLOW)
+        exitmessage = draw_text("Press BACKSPACE to exit", DESCRIPTION_FONT, YELLOW)
         exitrect = exitmessage.get_rect(center=(SCREEN_SIZE[0] // 2, 700))
         self.display.blit(exitmessage, exitrect)
 
-        results1 = draw_text(to_be_printed[0], descriptionfont, YELLOW)
+        results1 = draw_text(TO_BE_PRINTED[0], DESCRIPTION_FONT, YELLOW)
         results1rect = results1.get_rect(center=(SCREEN_SIZE[0] // 2, 200))
         self.display.blit(results1, results1rect)
 
-        results2 = draw_text(to_be_printed[1], descriptionfont, YELLOW)
+        results2 = draw_text(TO_BE_PRINTED[1], DESCRIPTION_FONT, YELLOW)
         results2rect = results2.get_rect(center=(SCREEN_SIZE[0] // 2, 300))
         self.display.blit(results2, results2rect)
 
-        results3 = draw_text(to_be_printed[2], descriptionfont, YELLOW)
+        results3 = draw_text(TO_BE_PRINTED[2], DESCRIPTION_FONT, YELLOW)
         results3rect = results3.get_rect(center=(SCREEN_SIZE[0] // 2, 400))
         self.display.blit(results3, results3rect)
 
-        results4 = draw_text(to_be_printed[3], descriptionfont, YELLOW)
+        results4 = draw_text(TO_BE_PRINTED[3], DESCRIPTION_FONT, YELLOW)
         results4rect = results4.get_rect(center=(SCREEN_SIZE[0] // 2, 500))
         self.display.blit(results4, results4rect)
 
-        results5 = draw_text(to_be_printed[4], descriptionfont, YELLOW)
+        results5 = draw_text(TO_BE_PRINTED[4], DESCRIPTION_FONT, YELLOW)
         results5rect = results5.get_rect(center=(SCREEN_SIZE[0] // 2, 600))
         self.display.blit(results5, results5rect)
 
@@ -140,11 +141,15 @@ class Questionnaire(Pages):
     page for the GUI
 
     Instance Attributes:
-        - current_error_message: The
+        - current_error_message: Error message to be displayed
+        - display: screen that will display questionnaire
+        - state_manager: object that will handle transition between pages
     """
     current_error_message: str
+    display: pygame.Surface
+    state_manager: StateManager
 
-    def __init__(self, display, state_manager):
+    def __init__(self, display: pygame.Surface, state_manager: StateManager) -> None:
         Pages.__init__(self, display, state_manager)
         self.current_error_message = ''
 
@@ -152,41 +157,43 @@ class Questionnaire(Pages):
         """Display and update the questionnaire page for the GUI"""
         self.display.fill(BLACK)
         text1surface = draw_text("Please answer the following questions "
-                                 "(press enter for each text box entry)", descriptionfont, YELLOW)
+                                 "(press enter for each text box entry)", DESCRIPTION_FONT, YELLOW)
         text1rect = text1surface.get_rect(center=(SCREEN_SIZE[0] // 2, 10))
         self.display.blit(text1surface, text1rect)
 
-        question1surf = draw_text("Enter your preferred release year of movies", descriptionfont,
+        question1surf = draw_text("Enter your preferred release year of movies", DESCRIPTION_FONT,
                                   YELLOW)
         question1rect = question1surf.get_rect(center=(SCREEN_SIZE[0] // 2, 120))
         self.display.blit(question1surf, question1rect)
 
-        question2surf = draw_text("Enter your preferred runtime (minutes)", descriptionfont, YELLOW)
+        question2surf = draw_text("Enter your preferred runtime (minutes)", DESCRIPTION_FONT,
+                                  YELLOW)
         question2rect = question2surf.get_rect(center=(SCREEN_SIZE[0] // 2, 200))
         self.display.blit(question2surf, question2rect)
 
-        question3surf = draw_text("Enter your preferred genre", descriptionfont, YELLOW)
+        question3surf = draw_text("Enter your preferred genre", DESCRIPTION_FONT, YELLOW)
         question3rect = question3surf.get_rect(center=(SCREEN_SIZE[0] // 2, 280))
         self.display.blit(question3surf, question3rect)
 
         question4surf = draw_text("Enter your preferred director (optional, press enter "
-                                  "in empty entry)", descriptionfont, YELLOW)
+                                  "in empty entry)", DESCRIPTION_FONT, YELLOW)
         question4rect = question4surf.get_rect(center=(SCREEN_SIZE[0] // 2, 360))
         self.display.blit(question4surf, question4rect)
 
         question5surf = draw_text("Enter your 3 preferred actors (optional, press enter "
-                                  "in empty entry)", descriptionfont, YELLOW)
+                                  "in empty entry)", DESCRIPTION_FONT, YELLOW)
         question5rect = question5surf.get_rect(center=(SCREEN_SIZE[0] // 2, 440))
         self.display.blit(question5surf, question5rect)
 
-        text2surf = draw_text("Press ESC to restart/Press TAB to continue", descriptionfont, YELLOW)
+        text2surf = draw_text("Press ESC to restart/Press TAB to continue",
+                              DESCRIPTION_FONT, YELLOW)
         text2rect = text2surf.get_rect(center=(SCREEN_SIZE[0] // 2, 550))
         self.display.blit(text2surf, text2rect)
 
         MANAGER.draw_ui(self.display)
 
         if self.current_error_message:
-            errorsurf = draw_text(self.current_error_message, descriptionfont, YELLOW)
+            errorsurf = draw_text(self.current_error_message, DESCRIPTION_FONT, YELLOW)
             errorrect = errorsurf.get_rect(center=(SCREEN_SIZE[0] // 2, 600))
             self.display.blit(errorsurf, errorrect)
 
@@ -216,7 +223,7 @@ class Start(Pages):
         self.display.fill(BLACK)
 
         keys = pygame.key.get_pressed()
-        titlesurface = draw_text("IMDb Movie Recommendation System", titlefont,
+        titlesurface = draw_text("IMDb Movie Recommendation System", TITLE_FONT,
                                  YELLOW)
         titlerect = titlesurface.get_rect(center=(SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 8))
         self.display.blit(titlesurface, titlerect)
@@ -228,12 +235,12 @@ class Start(Pages):
         collection = description.splitlines()
         curr_pos = 300
         for words in collection:
-            word_surface = draw_text(words, descriptionfont, YELLOW)
+            word_surface = draw_text(words, DESCRIPTION_FONT, YELLOW)
             word_rect = word_surface.get_rect(center=(SCREEN_SIZE[0] // 2, curr_pos))
             self.display.blit(word_surface, word_rect)
             curr_pos += 30
             self.display.blit(word_surface, word_rect)
-        continue_surface = draw_text("Press SPACE to continue", descriptionfont, YELLOW)
+        continue_surface = draw_text("Press SPACE to continue", DESCRIPTION_FONT, YELLOW)
         continue_rect = continue_surface.get_rect(center=(SCREEN_SIZE[0] // 2, curr_pos + 100))
         self.display.blit(continue_surface, continue_rect)
 
@@ -262,7 +269,7 @@ class Main:
     inputs: list[str]
     states: dict
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.screen = SCREEN
         self.state_manager = StateManager("start")
         self.start = Start(self.screen, self.state_manager)
@@ -288,8 +295,8 @@ class Main:
                     self.inputs.append(event.text)
                     self.states['questionnaire'].clear_error_message()
                 if event.type == pygame.KEYDOWN:
-                    if (event.key == pygame.K_ESCAPE and
-                            self.state_manager.get_state() == "questionnaire"):
+                    if (event.key == pygame.K_ESCAPE
+                            and self.state_manager.get_state() == "questionnaire"):
                         self.state_manager.set_state("start")
                         self.inputs.clear()
                     if event.key == pygame.K_TAB:
@@ -299,8 +306,8 @@ class Main:
                             self.questionnaire.errormessage(2)
                         else:
                             if is_integer(self.inputs[0]) and is_integer(self.inputs[1]):
-                                if (self.inputs[0] == '' or self.inputs[1] == '' or
-                                        self.inputs[2] == ''):
+                                if (self.inputs[0] == '' or self.inputs[1] == ''
+                                        or self.inputs[2] == ''):
                                     self.questionnaire.errormessage(3)
                                 else:
                                     self.outputs["year"] = self.inputs[0]
@@ -311,7 +318,7 @@ class Main:
                                     self.outputs["actor2"] = self.inputs[5]
                                     self.outputs["actor3"] = self.inputs[6]
 
-                                    to_be_printed.extend(recommendation_system
+                                    TO_BE_PRINTED.extend(recommendation_system
                                                          ("data/imdb_top_1000.csv", self.outputs))
                                     self.state_manager.set_state("results")
                             else:
@@ -325,3 +332,18 @@ class Main:
             self.states[self.state_manager.get_state()].run()
 
             pygame.display.update()
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
+
+    import python_ta
+    import python_ta.contracts
+
+    python_ta.check_all(config={
+        'extra-imports': ['pygame', 'sys', 'pygame_gui', 'weighted_decision', 'Any'],
+        'allowed-io': [],
+        'max-line-length': 120,
+    })
